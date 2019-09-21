2643a083d44c4b63c65bff988e4fa5b9b581e047
Sivanesh Logandurai <sivaneshl@virtusa.com>
2019-09-21 22:10:21 +0530

    data_analysis using pandas & matplotlib

--- data_analysis.py
+++ data_analysis.py
@@ -26,3 +26,20 @@ plt.title('Why do citizens use our website?')
 plt.axis("equal")
 plt.show()
 
+
+by_day = df.groupby('visit_date').first_name.count()
+# print(by_day.index, by_day.values)
+plt.figure(figsize=(16, 4))
+ax = plt.subplot()
+plt.plot(by_day.values, marker='o', markersize=8, linewidth=3)
+plt.ylabel("Number of visits")
+ax.set_xticks(range(len(by_day))[::8])
+ax.set_xticklabels(by_day.index[::8], rotation=45)
+plt.show()
+
+# histogram
+plt.hist(df.age, range=(10, 65), bins=11, edgecolor='white')
+plt.xlabel("Visitor Age")
+plt.ylabel("Number of Visitors")
+plt.title("Histogram of Visitor Age")
+plt.show()
